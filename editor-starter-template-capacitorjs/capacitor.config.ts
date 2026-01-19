3680a1670a6501d3e25c52c82e39677301280380
PhaserEditor2D <developers@phasereditor2d.com>
2026-01-19 13:32:46 -0500

    Update capacitor and webpack.

--- editor-starter-template-capacitorjs/capacitor.config.ts
+++ editor-starter-template-capacitorjs/capacitor.config.ts
@@ -1,10 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.phasereditor2d.marketplace.template',
   appName: 'phasereditor2d-capacitorjs-template',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: 'dist'
 };
 
 export default config;
